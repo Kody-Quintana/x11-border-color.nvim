cb363ef98753222876da45e6cf613615a30a30c2
Kody Quintana <Quintana.Kody@gmail.com>
2023-12-17 15:39:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# x11-border-color.nvim
+
+## Neovim plugin to change the border color of your terminal to indicate what mode neovim is in.
+
+Inspired by `chwb` from [wmutils](https://github.com/wmutils/core).
+This uses [LuaJIT FFI](https://luajit.org/ext_ffi.html) to call some C functions from [XCB](https://xcb.freedesktop.org/) directly to change the window's border color.
+This is made to work with [bspwm](https://github.com/baskerville/bspwm), but can probably work with other X11 WMs.
+
+Requires: <sup><sub>(You probably already have these installed)</sub></sup>
+
+  * `libxcb`
+  * `xcb-utils`
+
+
+
+![preview](https://github.com/Kody-Quintana/x11-border-color.nvim/assets/35752790/803d37c8-1fe8-461b-b720-94eeb33d0a51)
+
+## Install
+```lua
+use 'Kody-Quintana/x11-border-color.nvim'
+```
+
+## Configuration:
+
+```lua
+require("x11-border-color").setup({
+  normal_color = "#2cba1f",
+  insert_color = "#e21855",
+  --restore_color = "#FFFFFF",  -- If using bspwm, the default restore_color is the output of 'bspc config focused_border_color'
+})
+```
---
+++ lua/x11-border-color/init.lua
@@ -0,0 +1,147 @@
+local M = {}
+
+M.setup = function(config)
+  if config == nil then
+    config = {}
+  end
+
+  if not jit then
+    return nil
+  end
+
+  local ffi = require('ffi')
+
+  local function msg(message)
+    local prefix = '[x11-border-color]: '
+    print(prefix .. message)
+  end
+
+  local loaded_libxcb, xcb = pcall(ffi.load, 'xcb')
+  if not loaded_libxcb then
+    msg('failed to load xcb')
+    return nil
+  end
+  local loaded_libxcb_util, xcb_util = pcall(ffi.load, 'xcb-util')
+  if not loaded_libxcb_util then
+    msg('failed to load xcb-util')
+    return nil
+  end
+
+  if config.restore_color == nil then
+
+    -- bspwm special default case:
+    -- If bspwm is running, and no custom restore_color is specified, then assume
+    -- we want to set color back to whatever the bspwm focused border color is
+    local bspwm_focused_border_color = vim.fn.system({'bspc', 'config', 'focused_border_color'})
+    if vim.v.shell_error == 0 then
+        config.restore_color = bspwm_focused_border_color
+
+    -- TODO grab the current border color before nvim opens?
+    else
+      config.restore_color = "#FFFFFF"
+    end
+  end
+
+  if config.insert_color == nil then
+    config.insert_color = "#e21855"
+  end
+  if config.normal_color == nil then
+    config.normal_color = "#2cba1f"
+  end
+
+  local window_id_env_var = os.getenv('WINDOWID')
+  if window_id_env_var == '' or window_id_env_var == nil then
+    msg('WINDOWID env var not set')
+    return nil
+  end
+  local window = tonumber(window_id_env_var)
+  if window == nil then
+    msg('invalid WINDOWID env var: "' .. window_id_env_var .. '"')
+    return nil
+  end
+
+  -- Overrides `BorderPixmap`. A pixmap of undefined size filled with the specified
+  -- border pixel is used for the border. Range checking is not performed on the
+  -- border-pixel value, it is truncated to the appropriate number of bits.
+  local XCB_CW_BORDER_PIXEL = 8
+
+  ffi.cdef[[
+    typedef struct xcb_connection_t xcb_connection_t;
+
+    typedef uint32_t xcb_window_t;
+
+    typedef struct {
+      unsigned int sequence;
+    } xcb_void_cookie_t;
+
+    xcb_void_cookie_t xcb_change_window_attributes(
+      xcb_connection_t *c,
+      xcb_window_t      window,
+      uint32_t          value_mask,
+      const void       *value_list
+    );
+
+    xcb_connection_t *xcb_connect(const char *displayname, int *screenp);
+
+    int xcb_connection_has_error(xcb_connection_t *c);
+
+    void xcb_aux_sync(xcb_connection_t *c);
+
+    void xcb_disconnect(xcb_connection_t *c);
+  ]]
+
+  local value_list = ffi.new('uint32_t[3]')
+
+  local function set_border_color(color_hex_str)
+    if os.getenv('DISPLAY') == nil then
+      return nil
+    end
+
+    local color_int = tonumber(color_hex_str:gsub('#', ''), 16)
+    if color_int == nil then
+      msg('invalid color: "' .. color_hex_str .. '"')
+      return nil
+    end
+    value_list[0] = color_int
+
+    local conn = xcb.xcb_connect(nil, nil)
+    if xcb.xcb_connection_has_error(conn) ~= 0 then
+      msg('something went wrong connecting with xcb')
+      return nil
+    end
+
+    xcb.xcb_change_window_attributes(conn, window, XCB_CW_BORDER_PIXEL, value_list);
+    xcb_util.xcb_aux_sync(conn)
+    xcb.xcb_disconnect(conn)
+  end
+
+  vim.api.nvim_create_autocmd('InsertEnter', {
+    callback = function()
+      set_border_color(config.insert_color)
+    end
+  })
+
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    callback = function()
+      set_border_color(config.normal_color)
+    end
+  })
+
+  vim.api.nvim_create_autocmd({'FocusGained', 'VimEnter'}, {
+    callback = function()
+      if vim.api.nvim_get_mode().mode == 'i' then
+        set_border_color(config.insert_color)
+      else
+        set_border_color(config.normal_color)
+      end
+    end
+  })
+
+  vim.api.nvim_create_autocmd('VimLeave', {
+    callback = function()
+      set_border_color(config.restore_color)
+    end
+  })
+end
+
+return M
